863a462c23dfca14e076b731025f845da16de697
tjay099 <trayatjay@gmail.com>
2026-01-20 02:41:03 +0800

    code

---
+++ PYTHON_PACKAGE/PART1.py
@@ -0,0 +1,20 @@
+#firstdemo
+print ("Hello World")
+
+input ( "Enter your name : ")
+
+print ( "Carl said 'YOLO_BE_HAPPY!!!'"  )
+
+print ('YOLO')
+
+print ( "Carl said \"Hindi na ako natutuwa!!!!\"" )
+print ('Hello Japan')
+
+print('hello japan')
+
+print(input("Enter your pin : "))
+
+print('Hello Brazil Single Qoute')
+print("hello PH double Quote")
+print('Xarl said "HELOO JAVASKI SINGLE QUOTE"')
+print(" CARL SAID HELLO ANIME DOUBLE QUOTE'")

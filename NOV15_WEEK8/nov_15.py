9d338bc0a4a5476601d550cf9e6e7d7de1a084cb
anchtkenn-tech <rainkenneth03@gmail.com>
2026-01-20 18:50:39 +0800

    Delete NOV15_WEEK8 directory

--- NOV15_WEEK8/nov_15.py
+++
@@ -1,7 +0,0 @@
-print(welcome_messages())
-
-#Function with ParaMETER, and returndef welcome_messages():
-#Function with ParaMETER, and returnd f welcome_messages():
-def welcome_messages(message):
-    for i in range(10,0,-1):
-        message = message + " " + str(i)
